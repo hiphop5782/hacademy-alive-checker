600ff78af5a792318b79e43f50b95f1f31418808
Hacademy <zdlsz@hanmail.net>
2023-01-25 19:56:13 +0900

    Update server-alive-checker.py

--- server-alive-checker.py
+++ server-alive-checker.py
@@ -1,22 +1,30 @@
-# server check
 import requests
-
-uri = "https://www.sysout.co.kr";
-response = requests.get(uri, verify=False)
-if response.status_code == 200:
-    print("Server working now")
-    exit(0)
-
-# telegram bot send message
-
 import telegram as tel
 import asyncio
+try:
+    # server check
+    uri = "https://www.sysout.co.kr";
+    response = requests.get(uri, verify=False)
+    if response.status_code == 200:
+        print("Server working now")
+        exit(0)
 
-bot = tel.Bot(token="5818121143:AAGx5P6vYRNLmrmTfs1ym2Cfa5Mw_0IIKrU")
-chat_id = 5492413521
+    # telegram bot send message
+    bot = tel.Bot(token="5818121143:AAGx5P6vYRNLmrmTfs1ym2Cfa5Mw_0IIKrU")
+    chat_id = 5492413521
 
-asyncio.run(
-    bot.sendMessage(chat_id=chat_id, text=f"서버 상태 : {response.status_code}")
-)
-print("Server not working now")
-exit(-1)
+    asyncio.run(
+        bot.sendMessage(chat_id=chat_id, text=f"서버 상태 : {response.status_code}")
+    )
+    print("Server not working now")
+    exit(-1)
+except:
+    # telegram bot send message
+    bot = tel.Bot(token="5818121143:AAGx5P6vYRNLmrmTfs1ym2Cfa5Mw_0IIKrU")
+    chat_id = 5492413521
+
+    asyncio.run(
+        bot.sendMessage(chat_id=chat_id, text=f"Alive checker 실행 오류")
+    )
+    print("Alive checker error)
+    exit(-2)

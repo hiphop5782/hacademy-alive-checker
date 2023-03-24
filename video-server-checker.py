31af51183c152e74826ce8ce936f45ce96c8379f
Hacademy <zdlsz@hanmail.net>
2023-03-24 18:23:55 +0900

    Create video-server-checker.py

---
+++ video-server-checker.py
@@ -0,0 +1,36 @@
+import requests
+import telegram as tel
+import asyncio
+import sys
+
+def main():
+    try:
+        # server check
+        uri = "http://khds-c.iptime.org:30000/play/1";
+        response = requests.get(uri, verify=False)
+        if response.status_code == 200:
+            print("Server working now")
+            return True
+
+        # telegram bot send message
+        bot = tel.Bot(token="5818121143:AAGx5P6vYRNLmrmTfs1ym2Cfa5Mw_0IIKrU")
+        chat_id = 5492413521
+
+        asyncio.run(
+            bot.sendMessage(chat_id=chat_id, text=f"비디오 서버 : {response.status_code}")
+        )
+        print("Server not working now")
+        return False
+    except:
+        # telegram bot send message
+        bot = tel.Bot(token="5818121143:AAGx5P6vYRNLmrmTfs1ym2Cfa5Mw_0IIKrU")
+        chat_id = 5492413521
+
+        asyncio.run(
+            bot.sendMessage(chat_id=chat_id, text=f"비디오 서버 오류 발생")
+        )
+        print("Alive checker error")
+        raise
+
+if __name__ == "__main__":
+    main()
